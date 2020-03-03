0f2e3c2c1df0dd93a49f46857d7e776e89ae8960
Michael <z5259500@ad.unsw.edu.au>
2020-03-03 17:22:55 +1100

    Added test function names

---
+++ src/channel_test.py
@@ -0,0 +1,32 @@
+import channel
+import pytest
+from error import InputError
+from error import AccessError
+
+def test_channel_join():
+
+
+def test_channel_addowner():
+
+
+def test_channel_removeowner():
+
+
+def test_channel_join_InputError():
+
+
+def test_channel_addowner_InputError():
+
+
+def test_channel_remove_InputError():
+
+
+def test_channel_join_AccessError():
+
+
+def test_channel_addowner_AccessError():
+
+
+def test_channel_removeowner_AccessError():
+
+	
