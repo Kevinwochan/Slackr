e705662bcfeee668b6d21f3549e8723f2b839afb
kevin <kevinwochan@gmail.com>
2020-03-03 13:31:28 +1100

    implemented tests for auth

---
+++ src/auth_test.py
@@ -0,0 +1,72 @@
+import pytest
+from error import InputError
+import auth
+
+# Registration
+def test_register_email_invalid():
+    with pytest.raises(InputError) as e:
+        assert auth_register(1234, 'password')
+    with pytest.raises(InputError) as e:
+        assert auth_register('@unsw.edu.au', 'password')
+    with pytest.raises(InputError) as e:
+        assert auth_register('username', 'password')
+
+def test_register_email_in_use():
+    new_user = auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
+    with pytest.raises(InputError) as e:
+        auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
+
+def test_register_password_too_short():
+    with pytest.raises(InputError) as e:
+        auth_register('z55555555@unsw.edu.au', '12345', 'fname', 'lname')
+    with pytest.raises(InputError) as e:
+        auth_register('z55555555@unsw.edu.au', '', 'fname', 'lname')
+
+def test_register_name_length():
+    with pytest.raises(InputError) as e:
+        auth_register('z55555555@unsw.edu.au', 'password', '1', 'lname')
+    
+    with pytest.raises(InputError) as e:
+        auth_register('z55555555@unsw.edu.au', 'password', '1'*51, 'lname')
+    
+    with pytest.raises(InputError) as e:
+        auth_register('z55555555@unsw.edu.au', 'password', 'fname', '1')
+   
+    with pytest.raises(InputError) as e:
+        auth_register('z55555555@unsw.edu.au', 'password', 'fname', '1'*51)
+
+# Login
+def test_login_email_invalid():
+    with pytest.raises(InputError) as e:
+        assert auth_login(1234, 'password')
+    with pytest.raises(InputError) as e:
+        assert auth_login('@unsw.edu.au', 'password')
+    with pytest.raises(InputError) as e:
+        assert auth_login('username', 'password')
+
+def test_login_no_user_found():
+    new_user = auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
+    with pytest.raises(InputError) as e:
+        auth_login('z5555555@unsw.edu.au', 'password')
+
+def test_login_password_incorrect():
+    new_user = auth_register('z5555555@unsw.edu.au', 'password')
+    with pytest.raises(InputError) as e:
+        auth_login('z555555@unsw.edu.au', 'incorrect password')
+
+
+def test_logout():
+    new_user = auth_register('z5555555@unsw.edu.au', 'password')
+    login = auth_login('z555555@unsw.edu.au', 'incorrect password')
+    assert 'token' in login
+    assert auth_logout(login['token']) == {'is_success' : True}
+
+
+def test_valid_credentials();
+    user = auth_register('z5555555@unsw.edu.au', 'password', 'first', 'last')
+    assert 'u_id' in user
+    assert type(user['u_id']) is string
+    assert 'token' in user
+    assert type(user['token']) is string
+
+
