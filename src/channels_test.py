947e4473b3ac36f5490a98e5fcd0aa842cbc64ef
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2020-03-04 22:15:04 +1100

    added some comments to plan which tests ill write

--- src/channels_test.py
+++ src/channels_test.py
@@ -3,6 +3,33 @@ from channels import channels_list, channels_listall, channels_create
 from error import InputError
 import pytest
 
+'''
+tests:
+create channel
+    test - invalid token, invalid name
+    test return type
+    test invalid
+    create private and public
+    create 2 channels with the same name - error
+    create channels with name too long
+    create channel with empty name
+channels_list
+    invalid token
+    create channels
+    list channels
+    check return type
+    create another user, - list and check that dic channel isnt test_channels_create
+    2nd user should not be able to list first channel by default - not a member yet
+    create 2 channels and check
+    default? - willl there already be channels?
+channels_listall
+    invalid token
+    create channel as one user
+    create 2nd channel as other user
+        check list is not the same
+    check listall is the same (is this an assumption)
+    create private channel- check listall doesnt change
+'''
 def test_channels_create():
     test_user = auth_register("testemail@gmail.com", "1234567", "John", "Smith")
     channels_create(test_user['token'], 'name', True)
