c7b196bcb87dea8faff07e08f94fa634f3ca1358
kevinwochan <kevinwochan@gmail.com>
2020-03-05 01:10:28 +1100

    added docstring

--- src/auth_test.py
+++ src/auth_test.py
@@ -5,6 +5,9 @@ from auth import auth_login, auth_logout, auth_register
 
 # Registration
 def test_register_email_invalid():
+    """Tests that registration throws an input error for invalid emails, invalid emails are defined by 
+    https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
+    """
     with pytest.raises(InputError):
         assert auth_register('1234', 'password', 'placeholder_first_name',
                              'placeholder_last_name')
@@ -20,17 +23,16 @@ def test_register_email_invalid():
 
 
 def test_register_email_in_use():
+    """Tests that registration throws an input error when an email is used to create two accounts"""
     auth_register('z55555555@unsw.edu.au', 'password',
                   'placeholder_first_name', 'placeholder_last_name')
-    with pytest.raises(InputError):
-        auth_register('z55555555@unsw.edu.au', 'password',
-                      'placeholder_first_name', 'placeholder_last_name')
     with pytest.raises(InputError):
         auth_register('z55555555@unsw.edu.au', 'password1',
                       'placeholder_first_name1', 'placeholder_last_name1')
 
 
 def test_register_password_too_short():
+    """Tests that registration throws an input error when a password is shorter than 6 characters"""
     with pytest.raises(InputError):
         auth_register('z55555555@unsw.edu.au', '12345',
                       'placeholder_first_name', 'placeholder_last_name')
@@ -40,6 +42,7 @@ def test_register_password_too_short():
 
 
 def test_register_name_length():
+    """Tests that registration throws an input error when the name entered is less than 1 character or more than 50 characters"""
     with pytest.raises(InputError):
         auth_register('z55555555@unsw.edu.au', 'password', '',
                       'placeholder_last_name')
@@ -58,6 +61,7 @@ def test_register_name_length():
 
 
 def test_register_return_unique():
+    """Tests that registration generates unique tokens """
     user1 = auth_register('z55555555@unsw.edu.au', 'password',
                           'placeholder_first_name', 'placeholder_last_name')
     user2 = auth_register('z44444444@unsw.edu.au', 'password',
@@ -67,17 +71,19 @@ def test_register_return_unique():
 
 
 def test_register_valid_credentials():
-    new_user = auth_register('z5555555@unsw.edu.au', 'password', 'first',
-                             'last')
+    """Tests that registration works using valid information and everything i"""
+    new_user = auth_register('z5555555@unsw.edu.au', 'password', 'Michael',
+                             'Liang')
     assert isinstance(new_user, dict)
     assert 'u_id' in new_user
-    assert isinstance(new_user['u_id'], str)
+    assert isinstance(new_user['u_id'], int)
     assert 'token' in new_user
     assert isinstance(new_user['token'], str)
 
 
 # Login
 def test_login_email_invalid():
+    """Tests that logging in throws an input error when a invalid email is used"""
     with pytest.raises(InputError):
         assert auth_login('1234', 'password')
     with pytest.raises(InputError):
@@ -89,24 +95,27 @@ def test_login_email_invalid():
 
 
 def test_login_no_user_found():
+    """Tests that logging in throws an error when no account exists with that email"""
     with pytest.raises(InputError):
         auth_login('z5555555@unsw.edu.au', 'password')
 
 
 def test_login_password_incorrect():
+    """Tests that logging in throws an error when the password is incorrect"""
     auth_register('z5555555@unsw.edu.au', 'password', 'placeholder_first_name',
                   'placeholder_last_name')
     with pytest.raises(InputError):
         auth_login('z5555555@unsw.edu.au', 'incorrect password')
 
 
-def test_login_return_object():
+def test_login_valid_credentials():
+    """Tests logging in with valid information returns a dictionary described in the spec"""
     new_user = auth_register('z55555555@unsw.edu.au', 'password',
                              'placeholder_first_name', 'placeholder_last_name')
     login = auth_login('z55555555@unsw.edu.au', 'password')
     assert isinstance(login, dict)
     assert 'u_id' in login
-    assert isinstance(login['u_id'], str)
+    assert isinstance(login['u_id'], int)
     assert 'token' in login
     assert isinstance(login['token'], str)
     assert login['u_id'] == new_user['u_id']
@@ -114,18 +123,21 @@ def test_login_return_object():
 
 # logout
 def test_logout_no_account():
-    assert auth_logout('not a valid token') == {'is_success': False}
+    """Tests logging out without a valid token"""
+    with pytest.raises(AccessError):
+        assert auth_logout('not a valid token') == {'is_success': False}
 
 
 def test_logout():
+    """Tests logging out after logging in"""
     new_user = auth_register('z5555555@unsw.edu.au', 'password',
                              'placeholder_first_name', 'placeholder_last_name')
     assert auth_logout(new_user['token']) == {'is_success': True}
 
 
 def test_logout_twice():
+    """Tests logging out twice using the same token"""
     new_user = auth_register('z5555555@unsw.edu.au', 'password',
                              'placeholder_first_name', 'placeholder_last_name')
     assert auth_logout(new_user['token']) == {'is_success': True}
-    with pytest.raises(AccessError):
-        assert auth_logout(new_user['token']) == {'is_success': False}
+    assert auth_logout(new_user['token']) == {'is_success': False}
