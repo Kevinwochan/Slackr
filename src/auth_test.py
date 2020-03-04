f789e8f201ab1c0eb6bfbb69e9e76d4e4c5458b5
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2020-03-04 20:08:13 +1100

    I commented out sections that dont seem to be running properly (syntax error and such) so that the can be double checked and edited I also added a test to test_logout which checks that the token has been correctly invalidated. I also removed a test that i added to test_registration_return_object, which checked that the length of u_id was <= 20. This was an error, I thought that the user handle and id were the same.

--- src/auth_test.py
+++ src/auth_test.py
@@ -1,5 +1,5 @@
 import pytest
-from error import InputError
+from error import InputError, AccessError
 from auth import auth_login, auth_logout, auth_register
 
 
@@ -43,18 +43,14 @@ def test_register_name_length():
 
 def test_registration_return_object():
     new_user = auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
+
+    ''' - Can you double check this?
     assert type(new_user) is dict
     assert new_user['u_id'] in dict
     assert type(new_user['u_id']) is str
     assert new_user['token'] in dict
     assert type(new_user['token']) is str
-
-    # Checking length of u_id is always  <= 20
-    long_user = auth_register('z55555551@unsw.edu.au', 'password', 'L'*50, 'N'*50)
-    long_id = long_user['u_id']
-    assert len(long_id) <= 20
-    # Checking u_id is lowercase
-    assert long_id.islower() == True
+    '''
 
 def test_registration_return_unique():
     user1 = auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
@@ -78,32 +74,37 @@ def test_login_no_user_found():
         auth_login('z5555555@unsw.edu.au', 'password')
 
 def test_login_password_incorrect():
-    new_user = auth_register('z5555555@unsw.edu.au', 'password')
+    new_user = auth_register('z5555555@unsw.edu.au', 'password', 'fname', 'lname')
     with pytest.raises(InputError) as e:
         auth_login('z5555555@unsw.edu.au', 'incorrect password')
 
 def test_login_return_object():
     new_user = auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
     login = auth_login('z55555555@unsw.edu.au', 'password')
+    ''' -Can you double check this
     assert type(login) is dict
     assert login['u_id'] in dict
     assert type(login['u_id']) is str
     assert login['token'] in dict
     assert type(login['token']) is str
     assert login['u_id'] == new_user['u_id']
+    '''
 
 # logout
 def test_logout():
-    new_user = auth_register('z5555555@unsw.edu.au', 'password')
+    new_user = auth_register('z5555555@unsw.edu.au', 'password', 'fname', 'lname')
     login = auth_login('z5555555@unsw.edu.au', 'password')
     assert 'token' in login
     assert auth_logout(login['token']) == {'is_success' : True}
-    #invalid token - should return false assumption?
-    #test if token has been invalidated
+    # Checking that auth_logout invalidates the token
+    with pytest.raises(AccessError) as e:
+        assert auth_logout(login['token']) == {'is_success' : True}
 
 def test_valid_credentials():
     user = auth_register('z5555555@unsw.edu.au', 'password', 'first', 'last')
+    ''' -Can you double check this
     assert 'u_id' in user
     assert type(user['u_id']) is string
     assert 'token' in user
     assert type(user['token']) is string
+    '''
