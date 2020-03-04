87889317845eb1ac17aa41899953f74bcaa57a28
kevinwochan <kevinwochan@gmail.com>
2020-03-05 00:02:12 +1100

    fixed dictionary key checking

--- src/auth_test.py
+++ src/auth_test.py
@@ -6,7 +6,6 @@ from auth import auth_login, auth_logout, auth_register
 # Registration
 def test_register_email_invalid():
     with pytest.raises(InputError) as e:
-        #Assumption: functions will always be given the correct type
         assert auth_register('1234', 'password', 'fname', 'lname')
     with pytest.raises(InputError) as e:
         assert auth_register('@unsw.edu.au', 'password', 'fname', 'lname')
@@ -16,7 +15,7 @@ def test_register_email_invalid():
         assert auth_register('', 'password', 'fname', 'lname')
 
 def test_register_email_in_use():
-    new_user = auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
+    auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
     with pytest.raises(InputError) as e:
         auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
     with pytest.raises(InputError) as e:
@@ -41,22 +40,28 @@ def test_register_name_length():
     with pytest.raises(InputError) as e:
         auth_register('z55555555@unsw.edu.au', 'password', 'fname', '1'*51)
 
-def test_registration_return_object():
+def test_register_return_object():
     new_user = auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
-
-    ''' - Can you double check this?
     assert type(new_user) is dict
-    assert new_user['u_id'] in dict
+    assert 'u_id' in new_user
     assert type(new_user['u_id']) is str
-    assert new_user['token'] in dict
+    assert 'token' in new_user
     assert type(new_user['token']) is str
-    '''
 
-def test_registration_return_unique():
+def test_register_return_unique():
     user1 = auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
     user2 = auth_register('z44444444@unsw.edu.au', 'password', 'fname', 'lname')
     assert user1['token'] != user2['token']
     assert user1['u_id'] != user2['u_id']
+
+def test_register_valid_credentials():
+    new_user = auth_register('z5555555@unsw.edu.au', 'password', 'first', 'last')
+    assert type(new_user) is dict
+    assert 'u_id' in new_user
+    assert type(new_user['u_id']) is str
+    assert 'token' in new_user
+    assert type(new_user['token']) is str
+
 
 # Login
 def test_login_email_invalid():
@@ -74,37 +79,28 @@ def test_login_no_user_found():
         auth_login('z5555555@unsw.edu.au', 'password')
 
 def test_login_password_incorrect():
-    new_user = auth_register('z5555555@unsw.edu.au', 'password', 'fname', 'lname')
+    auth_register('z5555555@unsw.edu.au', 'password', 'fname', 'lname')
     with pytest.raises(InputError) as e:
         auth_login('z5555555@unsw.edu.au', 'incorrect password')
 
 def test_login_return_object():
     new_user = auth_register('z55555555@unsw.edu.au', 'password', 'fname', 'lname')
     login = auth_login('z55555555@unsw.edu.au', 'password')
-    ''' -Can you double check this
     assert type(login) is dict
-    assert login['u_id'] in dict
+    assert 'u_id' in login
     assert type(login['u_id']) is str
-    assert login['token'] in dict
+    assert 'token' in login
     assert type(login['token']) is str
     assert login['u_id'] == new_user['u_id']
-    '''
+
 
 # logout
 def test_logout():
     new_user = auth_register('z5555555@unsw.edu.au', 'password', 'fname', 'lname')
-    login = auth_login('z5555555@unsw.edu.au', 'password')
-    assert 'token' in login
-    assert auth_logout(login['token']) == {'is_success' : True}
-    # Checking that auth_logout invalidates the token
-    with pytest.raises(AccessError) as e:
-        assert auth_logout(login['token']) == {'is_success' : True}
+    assert auth_logout(new_user['token']) == {'is_success' : True}
 
-def test_valid_credentials():
-    user = auth_register('z5555555@unsw.edu.au', 'password', 'first', 'last')
-    ''' -Can you double check this
-    assert 'u_id' in user
-    assert type(user['u_id']) is string
-    assert 'token' in user
-    assert type(user['token']) is string
-    '''
+def test_logout_twice():
+    new_user = auth_register('z5555555@unsw.edu.au', 'password', 'fname', 'lname')
+    assert auth_logout(new_user['token']) == {'is_success' : True}
+    with pytest.raises(AccessError) as e:
+        assert auth_logout(new_user['token']) == {'is_success' : False}
