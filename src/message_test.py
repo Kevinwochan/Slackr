472a058dd4609a21ac7419ad3d1988e7fcdae425
kevin <kevinwochan@gmail.com>
2020-03-05 02:48:14 +1100

    implemented tests for editing messages

--- src/message_test.py
+++ src/message_test.py
@@ -127,3 +127,72 @@ def test_removing_authored_message(new_channel_and_user):
     message = message_send(author['token'], new_channel_and_user['channel_id'],
                            'a')
     message_remove(author['token'], message['message_id'])
+
+
+# Editing
+def test_editing_invalid_messages(new_channel_and_user):
+    """ Tests that editing a message with an invalid message id throws an input error"""
+    with pytest.raises(InputError):
+        assert message_edit(new_channel_and_user['token'], 0,
+                            'new message to replace existing message')
+    with pytest.raises(InputError):
+        assert message_edit(new_channel_and_user['token'], -1,
+                            'new message to replace existing message')
+    with pytest.raises(InputError):
+        assert message_edit(new_channel_and_user['token'], 50,
+                            'new message to replace existing message')
+
+
+def test_editing_a_message_unauthorized_user(new_channel_and_user):
+    """Tests that an error is thrown when the user is not authorised to edit the message
+        A user is not authorised if they are not authorised to see the channel
+    """
+    message = message_send(new_channel_and_user['token'],
+                           new_channel_and_user['channel_id'], 'a')
+    unauthorized_user = auth_register("z6666666@unsw.edu.au", "password",
+                                      "placeholder_first_name1",
+                                      "placeholder_last_name1")
+
+    with pytest.raises(AccessError):
+        assert message_edit(unauthorized_user['token'], message['message_id'],
+                            'new message to replace existing message')
+
+
+def test_editing_a_message_neither_author_nor_owner(new_channel_and_user):
+    """Tests that an error is thrown when the user is not authorised to edit the message
+        A user is not authorised if they are
+             i not the author of the message and
+            ii: not an admin/owner of the chat
+    """
+    not_author = auth_register("z6666666@unsw.edu.au", "password",
+                               "placeholder_first_name1",
+                               "placeholder_last_name1")
+    message = message_send(new_channel_and_user['token'],
+                           new_channel_and_user['channel_id'], 'a')
+    channel_join(not_author['token'], new_channel_and_user['channel_id'])
+
+    with pytest.raises(AccessError):
+        assert message_edit(not_author['token'], message['message_id'],
+                            'new message to replace existing message')
+
+
+def test_editing_authored_message(new_channel_and_user):
+    """ Tests the successful edit of a message from a user that sent it"""
+    author = auth_register("z6666666@unsw.edu.au", "password",
+                           "placeholder_first_name1", "placeholder_last_name1")
+    channel_join(author['token'], new_channel_and_user['channel_id'])
+    message = message_send(author['token'], new_channel_and_user['channel_id'],
+                           'a')
+    message_edit(author['token'], message['message_id'],
+                 'new message to replace existing message')
+
+
+def test_editing_authored_message_with_empty_message(new_channel_and_user):
+    """ Tests replacing a message with an empty message"""
+    author = auth_register("z6666666@unsw.edu.au", "password",
+                           "placeholder_first_name1", "placeholder_last_name1")
+    channel_join(author['token'], new_channel_and_user['channel_id'])
+    message = message_send(author['token'], new_channel_and_user['channel_id'],
+                           'a')
+    with pytest.raises(InputError):
+        message_edit(author['token'], message['message_id'], '')
