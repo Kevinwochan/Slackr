4c2b8984e07f48e4dc792c6f255ee31c63db9b12
Michael <z5259500@ad.unsw.edu.au>
2020-03-04 20:16:01 +1100

    added assumption

--- src/channel_test.py
+++ src/channel_test.py
@@ -4,29 +4,28 @@ from error import InputError
 from error import AccessError
 
 def test_channel_join():
-
+	pass
 
 def test_channel_addowner():
-
+	pass
 
 def test_channel_removeowner():
-
+	pass
 
 def test_channel_join_InputError():
-
+	pass
 
 def test_channel_addowner_InputError():
-
+	pass
 
 def test_channel_remove_InputError():
-
+	pass
 
 def test_channel_join_AccessError():
-
+	pass
 
 def test_channel_addowner_AccessError():
-
+	pass
 
 def test_channel_removeowner_AccessError():
-
-	
+	pass
