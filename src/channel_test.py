e32734441ce10daad9d2f39d9ce319283ce904f4
Michael Liang <michael.liang1110@gmail.com>
2020-03-05 01:37:31 +1100

    starting tests

--- src/channel_test.py
+++ src/channel_test.py
@@ -1,3 +1,4 @@
+from auth import auth_register
 import channel
 import pytest
 from error import InputError
@@ -5,6 +6,8 @@ from error import AccessError
 
 
 def test_channel_join():
+	test_user = auth_register("z5555555@unsw.edu.au","password", "John", "Smith") 
+	channel.channel_join(test_user["token"],)
 	pass
 
 # Assumption that first person to join a channel is Owner of that channel
