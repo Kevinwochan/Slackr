955939e1052eef45bea94b921087f39863c1b007
Yinpo Zhu <z5212912@ad.unsw.edu.au>
2020-03-06 10:55:25 +1100

    Fix the small bug in list

--- src/other_users_all_test.py
+++ src/other_users_all_test.py
@@ -16,13 +16,14 @@ def setup_other():
     
     # set up the expected value
     expected_out = []
-    expected_out[0] = {}
+    expected_out.append({})
     expected_out[0]['u_id'] = andrew_profile['u_id']
     expected_out[0]['email'] = andrew_profile['email']
     expected_out[0]['name_first'] = andrew_profile['name_first']
     expected_out[0]['name_last'] = andrew_profile['name_last']
     expected_out[0]['handle_str'] = andrew_profile['handle_str']
     
+    expected_out.append({})
     expected_out[1]['u_id'] = chris_profile['u_id']
     expected_out[1]['email'] = chris_profile['email']
     expected_out[1]['name_first'] = chris_profile['name_first']
