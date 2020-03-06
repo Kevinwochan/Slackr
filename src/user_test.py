52c33904e7be8137eb6843bb965e517687b590aa
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2020-03-06 13:37:50 +1100

    added invalid token tests to all user functions

--- src/user_test.py
+++ src/user_test.py
@@ -3,7 +3,20 @@ from user import user_profile, user_profile_setname, user_profile_setemail, user
 from auth import auth_register
 from error import InputError, AccessError
 
+#Assumption: anyone can view anyones profile
+def test_profile_invalid_token(inv_token):
+    test_user = auth_register("testemail@gmail.com", "1234567", "John", "Smith")
+    with pytest.raises(AccessError) as e:
+        user_profile(inv_token, test_user['u_id'])
 
-def test_user_profile(inv_token):
-    print (inv_token)
-    assert inv_token == '12345'
+def test_setname_invalid_token(inv_token):
+    with pytest.raises(AccessError) as e:
+        user_profile_setname(inv_token, "John", "Smith")
+
+def test_setemail_invalid_token(inv_token):
+    with pytest.raises(AccessError) as e:
+        user_profile_setemail(inv_token, "testemail@gmail.com")
+
+def test_sethandle_invalid_token(inv_token):
+    with pytest.raises(AccessError) as e:
+        user_profile_sethandle(inv_token, 'hjacob')
