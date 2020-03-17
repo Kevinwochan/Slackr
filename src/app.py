77440487b6e10f83adfc720c85c31f5d5095c983
kevinwochan <kevinwochan@gmail.com>
2020-03-17 17:52:00 +1100

    starter code for flask

--- src/app.py
+++ src/app.py
@@ -4,11 +4,34 @@ import json
 
 app = Flask(__name__)
 
+'''
+a list of users.
+each user is a dictionary like:
+    user = {
+        'u_id': 2,
+        etc...
+        'email',
+        name_first, 
+        name_last, 
+        handle_str,
+        password,
+        username
+    }
+'''
+USERS = []
+CHANNELS = []
+'''
+a list of jwts generated from a user successffully logging in
+'''
+JWTS = []
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-# TODO: remmove this get method, get is only for testing
-@app.route('/auth/login', methods=['GET','POST'])
-def login():
-        return json.dumps(auth_login('', ''))
 
+@app.route('/auth/login', methods=['POST'])
+def login():
+    global JWTS
+    JWTS.append(auth_login('', ''))
+    return
