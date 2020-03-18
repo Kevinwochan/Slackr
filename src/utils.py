20611b9bcfe045e337754bce304dcef67ba3e47e
kevin <kevinwochan@gmail.com>
2020-03-19 01:44:11 +1100

    added todo reminder for verifying workspace reset pwermissions

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ from src.channel import CHANNELS
 
 def workspace_reset():
     ''' Deletes all Slackr information as if the website was just launched '''
+    # TODO: verify user is an owner of the slackr
     JWTS.clear()
     USERS.clear()
     CHANNELS.clear()
