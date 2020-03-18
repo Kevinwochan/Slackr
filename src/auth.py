c0e6b6fe8c4339f53dfa1504bb85253c0427262e
kevin <kevinwochan@gmail.com>
2020-03-19 01:02:54 +1100

    added global variables and data structure explanations

--- src/auth.py
+++ src/auth.py
@@ -1,3 +1,27 @@
+JWTS = []
+'''
+JWTS[u_id] = 'a valid token for u_id'
+a list of jwts generated from a user successffully logging in
+'''
+
+
+USERS = {}
+'''
+a list of users.
+each user is a dictionary like:
+    user = USERS[u_id]
+    user = {
+        'u_id': 2,
+        'email' : 'z5555555@unsw.edu.au',
+        'name_first': 'Hayden', 
+        'name_last' : 'Smith', 
+         etc...
+        handle_str,
+        password,
+        username
+    }
+'''
+
 def auth_login(email, password):
     return {
         'u_id': 1,
