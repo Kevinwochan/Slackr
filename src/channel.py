70e5148a0cb4a6cf2fcfa6359c778f8370fbd992
kevin <kevinwochan@gmail.com>
2020-03-20 17:56:39 +1100

    added helper funcitons and impelemented more channel functions

--- src/channel.py
+++ src/channel.py
@@ -1,101 +1,169 @@
 from src.channels import CHANNELS
 from src.error import InputError, AccessError
-
+from src.utils import check_token
+from src.auth import SLACKR_OWNER, USERS
 '''
     Helper functions for writing less code
 '''
-def is_valid_channel(channel_id)
+
+
+def is_valid_channel(channel_id):
     ''' returns true if the channel id is valid'''
-    return CHANNELS.get(channel_id, -1) != -1
+    return channel_id in CHANNELS
 
-def is_user_a_member(channel_id, u_id):
-    ''' returns true if the u_id is a member of the channel '''
-    for user in CHANNELS[channel_id]['all_members']
-        if user['u_id'] == u_id:
+
+def is_user_a_member(channel_id, user_id):
+    ''' returns true if the user_id is a member of the channel '''
+    for user in CHANNELS[channel_id]['all_members']:
+        if user['user_id'] == user_id:
             return True
     return False
 
-def is_user_a_owner(channel_id, u_id):
-    ''' returns true if the u_id is an owner of the channel '''
-    for user in CHANNELS[channel_id]['members']
-        if user['u_id'] == u_id and user['is_owner']:
+
+def is_user_a_owner(channel_id, user_id):
+    ''' returns true if the user_id is an owner of the channel '''
+    for user in CHANNELS[channel_id]['members']:
+        if user['user_id'] == user_id and user['is_owner']:
             return True
     return False
 
-def get_channel_owners(channel_id);
-    ''' returns a new list of owners of a channel '''
-    return [for user in CHANNELS[channel_id]['members'] if user['is_owner']]
 
-def get_channel_users(channel_id):
+def get_channel_owners(channel_id):
+    ''' returns a list of owners of a channel '''
+    return [
+        user for user in CHANNELS[channel_id]['members'] if user['is_owner']
+    ]
+
+
+def get_channel_members(channel_id):
     ''' returns a list of channel members '''
-    return  CHANNELS[channel_id]['members']
+    return CHANNELS[channel_id]['members']
+
 
 '''
     Main Channel functions
 '''
-def channel_invite(token, channel_id, u_id):
-    return {
-    }
+
+
+def channel_invite(token, channel_id, user_id):
+    return {}
+
 
 def channel_details(token, channel_id):
+    user_id = check_token(token)
+
     if not is_valid_channel(channel_id):
         raise InputError
-    # TODO: verify token
+
+    if not is_user_a_member(channel_id, user_id) and not is_user_a_owner(
+            channel_id, user_id):
+        raise AccessError
+
+    owner_members = []
+    all_members = []
+    for user_id in get_channel_owners(channel_id):
+        owner_members.append({
+            'user_id': user_id,
+            'name_first': USERS[user_id]['name_first'],
+            'name_last': USERS[user_id]['name_last']
+        })
+        all_members.append({
+            'user_id': user_id,
+            'name_first': USERS[user_id]['name_first'],
+            'name_last': USERS[user_id]['name_last']
+        })
+
+    for user_id in get_channel_members(channel_id):
+       all_members.append({
+            'user_id': user_id,
+            'name_first': USERS[user_id]['name_first'],
+            'name_last': USERS[user_id]['name_last']
+        }) 
     return {
-        'name': CHANNELS[channel_id]['name']
-        'owner_members': get_channel_owners(channel_id)
-        'all_members': get_channel_users(channel_id)
+        'name': CHANNELS[channel_id]['name'],
+        'owner_members': owner_members,
+        'all_members': get_channel_members(channel_id)
     }
 
+
 def channel_messages(token, channel_id, start):
-    if not is_valid_channel(channel_id)
+    '''
+        returns a dictionary containing:
+            messages: a list of messages starting with the most recent at index 0
+            start: the index of the starting message 
+            end: the index of the last message
+    '''
+    user_id = check_token(token)
+
+    if not is_valid_channel(channel_id):
         raise InputError
 
-    if start > len(CHANNELS[channel_id]['messages']):
-        raise InputError
-   
-    # TODO: verify token
+    if not is_user_a_member(channel_id, user_id) and not is_user_a_owner(
+            channel_id, user_id):
+        raise AccessError
+
     channel = CHANNELS[channel_id]
-    end = start+50
+    if start > len(channel['messages']):
+        raise InputError
+
+    end = start + 50
     if end > len(channel['messages']):
         end = -1
 
     return {
-        'messages': channel['messages'][start:start+50],
+        'messages': channel['messages'][start:start + 50],
         'start': start,
         'end': end,
     }
 
+
 def channel_leave(token, channel_id):
-    return {
-    }
+    return {}
+
 
 def channel_join(token, channel_id):
-    return {
-    }
 
-def channel_addowner(token, channel_id, u_id):
+    return {}
+
+
+def channel_addowner(token, channel_id, user_id):
     ''' 
-        makes a user a channel ower, 
-        if the user is not already a member the user is added to the channel first
+        Makes a user a channel ower, 
+        The token must be of a channel owner or the slackr owner
     '''
-    #TODO: authennticate token
+    owner_id = check_token(token)
+
     if not is_valid_channel(channel_id):
         raise InputError
 
-    if u_id in get_channel_owners(channel_id):
+    if not is_user_a_owner(channel_id, owner_id) and owner_id != SLACKR_OWNER:
+        raise AccessError
+
+    if is_user_a_owner(channel_id, user_id):
         raise InputError
 
-    if not u_id in get_channel_users(channel_id):
-        channel_join(token, channel_id, u_id)
+    if is_user_a_member(channel_id, user_id):
+        get_channel_members(channel_id).remove(user_id)
+    get_channel_owners(channel_id).append(user_id)
 
-    for user in get_channel_users(channel_id):
-        if user['u_id'] == u_id:
-            user['is_owner'] = True
+    return {}
 
-    return {
-    }
 
-def channel_removeowner(token, channel_id, u_id):
-    return {
-    }
+def channel_removeowner(token, channel_id, user_id):
+    '''
+        Makes a channel owner a regular channel member
+        the token must be one of the channel owners or the slackr owner
+    '''
+    owner_id = check_token(token)
+
+    if not is_valid_channel(channel_id):
+        raise InputError
+
+    if not is_user_a_owner(channel_id, owner_id) and owner_id != SLACKR_OWNER:
+        raise AccessError
+
+    if is_user_a_owner(channel_id, user_id):
+        raise InputError
+
+    get_channel_owners(channel_id).remove(user_id)
+    return {}
