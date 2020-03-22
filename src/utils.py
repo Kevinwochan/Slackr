47b01466b2a7e3b7688cfedd15bfec2686b50141
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2020-03-22 12:56:21 +1100

    moved auth helper functions from utils.py into auth_helper.py

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,11 @@
 '''
 Contains miscellaneous helper functions. These may be seperated into different files eventually
 '''
-
 #needs pip3 install pyjwt
 # Assumption: Users are logged out after a server restart (presuming they are not also unregistered)
-import re
 from jwt import encode, decode
-from error import AccessError, InputError
-from global_variables import get_valid_tokens, get_users
+from error import AccessError
+from global_variables import get_valid_tokens
 SECRET = 'F FOR HAYDEN'
 
 def generate_token(user_id):
@@ -46,90 +44,3 @@ def invalidate_token(token):
     except ValueError:
         raise AccessError(description="Token is already invalid")
     return True
-
-####################################################################################################
-##                                   ||Auth Helper Functions||                                    ##
-####################################################################################################
-# These functions check that inputs are valid according to the project specifications
-
-# helper functions
-def is_email_valid(email):
-    '''
-    Checks if email is valid, using the method described here:
-    https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
-
-    Returns True if email is valid, otherwise returns False.
-    '''
-    condition = re.search(r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email)
-
-    return bool(condition)
-
-def is_password_valid(password):
-    
-    '''
-    '[invalid if] Password entered is less than 6 characters long'
-
-    Returns True if password is valid, otherwise returns False
-    '''
-    condition = len(password) >= 6
-
-    return bool(condition)
-
-def is_name_valid(name):
-    '''
-    '[Invalid if] name is not between 1 and 50 characters inclusive in length'
-
-    Returns True if name is valid, otherwise returns False.
-    '''
-    condition_1 = len(name) >= 1
-    condition_2 = len(name) <= 50
-
-    return bool(condition_1 and condition_2)
-
-def is_email_unique(email):
-    '''
-    Checks if an email is already associated with a user in glob_users
-    '''
-    glob_users = get_users()
-
-
-    for user in glob_users.values():
-        if email == user['email']:
-            return False
-    return True
-
-def is_handle_unique(handle_str):
-    '''
-    Checks if a handle is already associated with a user in glob_users
-    '''
-    glob_users = get_users()
-
-    for user in glob_users.values():
-        if handle_str == user['handle_str']:
-            return False
-    return True
-
-def check_registration_inputs(email, password, name_first, name_last):
-    '''
-    Checks all inputs for registration raises the appropriate errors
-    '''
-    if not is_email_valid(email):
-        raise InputError(description="Invalid Email")
-    if not is_email_unique(email):
-        raise InputError(description="An account with this email has already been registered")
-    if not is_password_valid(password):
-        raise InputError(description="Password not strong enough")
-    if not is_name_valid(name_first):
-        raise InputError(description="First name must be between 1 and 50 characters long")
-    if not is_name_valid(name_last):
-        raise InputError(description="Last name must be between 1 and 50 characters long")
-
-def check_login_inputs(email):
-    '''
-    Checks all inputs for login raises the appropriate errors
-    '''
-    if not is_email_valid(email):
-        raise InputError(description="Invalid Email")
-
-    if is_email_unique(email):
-        raise InputError(description="This email has not been registered")
