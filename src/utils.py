8568020caf6a5629317704aa5dd3af54d96ba39e
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2020-03-22 13:48:34 +1100

    edited generate_token function. If the generated token is already stored in global_valid_tokens, it is not added. i.e. if a user logs in twice, only one instance of their token is stored

--- src/utils.py
+++ src/utils.py
@@ -11,11 +11,13 @@ SECRET = 'F FOR HAYDEN'
 def generate_token(user_id):
     '''
     Returns a JWT token based on the users id and a secret message.
+    if a user is already logged in, it does not add the token to curr_users
     '''
     curr_users = get_valid_tokens()
 
     token = encode({'id': user_id}, SECRET, algorithm='HS256').decode('utf-8')
-    curr_users.append(token)
+    if token not in curr_users:
+        curr_users.append(token)
     return token
 
 
