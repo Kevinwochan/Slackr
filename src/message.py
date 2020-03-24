729b2ab80a9bd813498c071317fc7dd5f4108077
Yinpo Zhu <z5212912@ad.unsw.edu.au>
2020-03-24 14:40:24 +1100

    send_message function with assumptions

--- src/message.py
+++ src/message.py
@@ -1,9 +1,37 @@
 from src.channels import CHANNELS
+from utils import check_token
+from error import AccessError, InputError
+import time
+
+message_id = 0
 
 
 def message_send(token, channel_id, message):
+
+    # Check if the token is valid and get the u_id
+    uid = check_token(token)
+
+    # Check if the message is valid
+    if len(message) > 1000:
+        raise InputError("Your message should be less than 1000")
+    
+    global message_id
+    message_id += 1
+    CHANNELS[channel_id]['messges'].append({
+        'message_id': message_id,
+        'timestamp': str(time.strftime("%Y%m%d%H%M")),
+        'message': message
+
+        # For the reacts, I assume it is a list of dictionary beacuse it is possible
+        # that more than one user could react to the message
+        'reacts': [{
+            'u_id':[]
+            'emoji':[]  
+        }]
+    })
+
     return {
-        'message_id': 1,
+        'message_id': message_id,
     }
 
 
