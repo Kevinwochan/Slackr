e832b8b1082aa845492569b289ed2d17fb2a7d08
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2020-03-26 18:19:46 +1100

    started standup_active function

--- src/standup.py
+++ src/standup.py
@@ -1,8 +1,9 @@
-import threading
-from utils import check_token
+from threading import Timer
+from utils import check_token, get_current_timestamp
 from global_variables import get_channels
 from error import InputError,AccessError
 from channel import is_valid_channel, is_user_a_member, is_user_a_owner
+#TODO: change get_message_id, check reacts
 #helper stuff
 global_standups = {}
 
@@ -29,11 +30,24 @@ def check_standup_inputs(channel_id, user_id):
     if not is_user_a_member(channel_id, user_id) and not is_user_a_owner(channel_id, user_id):
         raise AccessError(description='You are not a member of this channel')
 
-def get_message_id():
+def get_message_id(channel_id):
     '''
     placeholder function - used untill messages file is complete with method to generate message id's
+    #TODO: replace this with same method used in message.py (ideally by importing a function)
     '''
-    return -1
+    from message import message_id
+    global message_id
+    message_id += 1
+    return message_id
+
+
+def standup_end(channel_id):
+    glob_channels = get_channels()
+    glob_standups = get_standups()
+    glob_standups[channel_id]['message_id'] = get_message_id()
+    glob_channels[channel_id]['messages'].append(glob_standups.pop(channel_id))
+
+
 
 
 # functions
@@ -47,12 +61,15 @@ def standup_start(token, channel_id, length):
         raise InputError(description='A standup is already active in this channel')
     
     glob_standups = get_standups()
+    time_finish = get_current_timestamp() + length
+    #creating blank message with time_finish timestamp, and storing it in glob_standups
+    #has placeholder message_id
     global_standups[channel_id] = {
         ({
         'u_id': u_id
         'message_id': -1,
-        'timestamp': str(time.strftime("%Y%m%d%H%M")),
-        'message': message
+        'timestamp': time_finish,
+        'message': ''
         'reacts': [{
             'u_ids':[]
             'emoji':[]  
@@ -60,18 +77,26 @@ def standup_start(token, channel_id, length):
         'is_pined':False
     })
 
-
+    standup = Timer(length, standup_end, args=[channel_id])
+    standup.start()
     }
     return {
-        'time_finish': 1
+        'time_finish': time_finish
     }
 
 
 def standup_active(token, channel_id):
-    
+    u_id = check_token(token)
+    check_standup_inputs(channel_id, u_id)
+    is_active = is_standup_active(channel_id)
+    glob_standups = get_standups
+    try:
+        time_finish = get_standups[channel_id]['timestamp']
+    except KeyError:
+        time_finish = None
     return {
-        'is_active': False, 
-        'time_finish': 1
+        'is_active': is_active, 
+        'time_finish': 
     }
 
 
