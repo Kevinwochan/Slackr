5a8424b8562be2b1966a9d683479a39bd61dcd7c
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2020-03-26 18:53:25 +1100

    additional bug fixes

--- src/standup.py
+++ src/standup.py
@@ -30,7 +30,7 @@ def check_standup_inputs(channel_id, user_id):
     if not is_user_a_member(channel_id, user_id) and not is_user_a_owner(channel_id, user_id):
         raise AccessError(description='You are not a member of this channel')
 
-def get_message_id(channel_id):
+def get_message_id():
     '''
     placeholder function - used untill messages file is complete with method to generate message id's
     #TODO: replace this with same method used in message.py (ideally by importing a function)
@@ -85,11 +85,12 @@ def standup_active(token, channel_id):
     u_id = check_token(token)
     check_standup_inputs(channel_id, u_id)
     is_active = is_standup_active(channel_id)
-    glob_standups = get_standups
+
     try:
-        time_finish = get_standups(channel_id)['timestamp']
+        time_finish = get_standups()[channel_id]['timestamp']
     except KeyError:
         time_finish = None
+
     return {
         'is_active': is_active, 
         'time_finish': time_finish
