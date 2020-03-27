79a737158896a233f2f6eece984e028c8a61dde3
kevin <kevinwochan@gmail.com>
2020-03-28 00:52:52 +1100

    passing message tests

--- src/IntegrationTests/message_test.py
+++ src/IntegrationTests/message_test.py
@@ -3,7 +3,7 @@
 import pytest
 from src.error import AccessError, InputError
 from src.channels import channels_create
-from src.channel import channel_join
+from src.channel import channel_join, channel_invite
 from src.auth import auth_register
 from src.message import message_edit, message_remove, message_send
 
@@ -12,14 +12,14 @@ from src.message import message_edit, message_remove, message_send
 def new_user():
     """creates a new user"""
     return auth_register("z5555555@unsw.edu.au", "password",
-                         "placeholder_first_name", "placeholder_last_name")
+                         "first_name", "last_name")
 
 
 @pytest.fixture
 def new_channel_and_user(new_user):
     """creates a new user then a new channel and returns a merged dictionary"""
     new_channel = channels_create(new_user['token'],
-                                  "placeholder_channel_name", False)
+                                  "channel_name", False)
     return {**new_channel, **new_user}
 
 
@@ -50,8 +50,8 @@ def test_sending_message_without_channel_access(new_channel_and_user):
         they do not have access to, throws an error
     """
     unauthorized_user = auth_register("z6666666@unsw.edu.au", "password",
-                                      "placeholder_first_name1",
-                                      "placeholder_last_name1")
+                                      "first_name1",
+                                      "last_name1")
 
     with pytest.raises(AccessError):
         assert message_send(unauthorized_user['token'],
@@ -81,8 +81,8 @@ def test_sending_empty_message(new_channel_and_user):
 def test_sending_with_invalid_token(new_channel_and_user):
     """ Tests that an access error is thrown when an an invalid token is used """
     with pytest.raises(AccessError):
-        message_send('invalid token', new_channel_and_user['channel_id'], 'a')
-
+        message_send('invalid token', new_channel_and_user['channel_id'], 'a') 
+         
 
 # Removing
 def test_removing_invalid_messages(new_channel_and_user):
@@ -102,8 +102,8 @@ def test_removing_a_message_unauthorized_user(new_channel_and_user):
     message = message_send(new_channel_and_user['token'],
                            new_channel_and_user['channel_id'], 'a')
     unauthorized_user = auth_register("z6666666@unsw.edu.au", "password",
-                                      "placeholder_first_name1",
-                                      "placeholder_last_name1")
+                                      "first_name1",
+                                      "last_name1")
 
     with pytest.raises(AccessError):
         assert message_remove(unauthorized_user['token'],
@@ -117,11 +117,11 @@ def test_removing_a_message_neither_author_nor_owner(new_channel_and_user):
             ii: not an admin/owner of the chat
     """
     not_author = auth_register("z6666666@unsw.edu.au", "password",
-                               "placeholder_first_name1",
-                               "placeholder_last_name1")
+                               "first_name1",
+                               "last_name1")
     message = message_send(new_channel_and_user['token'],
                            new_channel_and_user['channel_id'], 'a')
-    channel_join(not_author['token'], new_channel_and_user['channel_id'])
+    channel_invite(new_channel_and_user['token'], new_channel_and_user['channel_id'], not_author['u_id'])
 
     with pytest.raises(AccessError):
         assert message_remove(not_author['token'], message['message_id'])
@@ -130,11 +130,20 @@ def test_removing_a_message_neither_author_nor_owner(new_channel_and_user):
 def test_removing_authored_message(new_channel_and_user):
     """ Tests the successful removal of a message from a user that sent it"""
     author = auth_register("z6666666@unsw.edu.au", "password",
-                           "placeholder_first_name1", "placeholder_last_name1")
-    channel_join(author['token'], new_channel_and_user['channel_id'])
+                           "first_name1", "last_name1")
+    channel_invite(new_channel_and_user['token'], new_channel_and_user['channel_id'], author['u_id'])
     message = message_send(author['token'], new_channel_and_user['channel_id'],
                            'a')
     message_remove(author['token'], message['message_id'])
+
+def test_removing_message_as_owner(new_channel_and_user):
+    ''' tests the sucessful removal of a message by the channel owner'''
+    author = auth_register("z6666666@unsw.edu.au", "password",
+                           "first_name1", "last_name1")
+    channel_invite(new_channel_and_user['token'], new_channel_and_user['channel_id'], author['u_id'])
+    message = message_send(author['token'], new_channel_and_user['channel_id'],
+                           'a')
+    message_remove(new_channel_and_user['token'], message['message_id'])
 
 
 def test_removing_with_invalid_token(new_channel_and_user):
@@ -166,8 +175,8 @@ def test_editing_a_message_unauthorized_user(new_channel_and_user):
     message = message_send(new_channel_and_user['token'],
                            new_channel_and_user['channel_id'], 'a')
     unauthorized_user = auth_register("z6666666@unsw.edu.au", "password",
-                                      "placeholder_first_name1",
-                                      "placeholder_last_name1")
+                                      "first_name1",
+                                      "last_name1")
 
     with pytest.raises(AccessError):
         assert message_edit(unauthorized_user['token'], message['message_id'],
@@ -181,11 +190,11 @@ def test_editing_a_message_neither_author_nor_owner(new_channel_and_user):
             ii: not an admin/owner of the chat
     """
     not_author = auth_register("z6666666@unsw.edu.au", "password",
-                               "placeholder_first_name1",
-                               "placeholder_last_name1")
+                               "first_name1",
+                               "last_name1")
     message = message_send(new_channel_and_user['token'],
                            new_channel_and_user['channel_id'], 'a')
-    channel_join(not_author['token'], new_channel_and_user['channel_id'])
+    channel_invite(new_channel_and_user['token'], new_channel_and_user['channel_id'], not_author['u_id'])
 
     with pytest.raises(AccessError):
         assert message_edit(not_author['token'], message['message_id'],
@@ -195,8 +204,8 @@ def test_editing_a_message_neither_author_nor_owner(new_channel_and_user):
 def test_editing_authored_message(new_channel_and_user):
     """ Tests the successful edit of a message from a user that sent it"""
     author = auth_register("z6666666@unsw.edu.au", "password",
-                           "placeholder_first_name1", "placeholder_last_name1")
-    channel_join(author['token'], new_channel_and_user['channel_id'])
+                           "first_name1", "last_name1")
+    channel_invite(new_channel_and_user['token'], new_channel_and_user['channel_id'], author['u_id'])
     message = message_send(author['token'], new_channel_and_user['channel_id'],
                            'a')
     message_edit(author['token'], message['message_id'],
@@ -204,12 +213,13 @@ def test_editing_authored_message(new_channel_and_user):
 
 
 def test_editing_authored_message_with_empty_message(new_channel_and_user):
-    """ Tests replacing a message with an empty message"""
+    """ Tests replacing a message with an empty message removes the messages"""
     author = auth_register("z6666666@unsw.edu.au", "password",
-                           "placeholder_first_name1", "placeholder_last_name1")
-    channel_join(author['token'], new_channel_and_user['channel_id'])
+                           "first_name1", "last_name1")
+    channel_invite(new_channel_and_user['token'], new_channel_and_user['channel_id'], author['u_id'])
     message = message_send(author['token'], new_channel_and_user['channel_id'],
                            'a')
+    message_edit(author['token'], message['message_id'], '')
     with pytest.raises(InputError):
         message_edit(author['token'], message['message_id'], '')
 
