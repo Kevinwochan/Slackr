cd34820e14c55f21de52ecb66f48421fc07c3bb5
Michael <michael.liang1110@gmail.com>
2020-03-29 16:30:14 +1100

    finished permission_change func

--- src/admin.py
+++ src/admin.py
@@ -3,7 +3,7 @@ Function to change user permissions between user and admin/owner.
 '''
 from src.error import InputError, AccessError
 from src.utils import check_token
-from src.global_variables import get_slackr_owners
+from src.global_variables import get_slackr_owners, get_users
 from src.channel import is_valid_user
 
 def permission_change(token, u_id, permission_id): #change a user's permissions on the server
@@ -13,8 +13,15 @@ def permission_change(token, u_id, permission_id): #change a user's permissions 
     if not is_valid_user(u_id):
         raise InputError
 
-    if permission_id != 1 and permission_id != 2: #1 = owner, 2 = member
+    if permission_id not in (1, 2):
         raise InputError
 
     if not user_id in get_slackr_owners():
         raise AccessError
+    
+    # all possible errors raised.
+    if permission_id == 1:
+        get_users()[u_id]['is_owner'] = True
+        
+    elif permission_id == 2:
+        get_users()[u_id]['is_owner'] = False
