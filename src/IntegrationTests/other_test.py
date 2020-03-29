7bd9dbcd0bae30c9e5fa7920f268dfd5ce13947e
kevinwochan <kevinwochan@gmail.com>
2020-03-29 11:16:26 +1100

    fixed user all test

--- src/IntegrationTests/other_test.py
+++ src/IntegrationTests/other_test.py
@@ -189,34 +189,24 @@ def test_users_all_with_invalid_token():
 
 
 # Normal
-def normal_test():
+def test_users_all_normal_test():
     # set up the user
     user_andrew = auth_register("andrewt@gmail.com", "password", "andrew",
                                 "taylor")
-    user_chris = auth_register("chrisc@gmail.com", "pilot", "chris", "chen")
+    user_chris = auth_register("chrisc@gmail.com", "pilotpassword", "chris", "chen")
 
     # Access the users info by andrew's token
-    users_card = users_all(user_andrew['token'])
+    users = users_all(user_andrew['token'])['users']
 
-    # To get the handle_str of each user
-    andrew_profile = user_profile(user_andrew['token'], user_andrew['u_id'])
-    chris_profile = user_profile(user_chris['token'], user_chris['u_id'])
+    assert len(users) == 2
 
-    # andrew's expected info
-    andrew_card = {}
-    andrew_card['u_id'] = user_andrew['u_id']
-    andrew_card['email'] = "andrewt@gmail.com"
-    andrew_card['name_first'] = "andrew"
-    andrew_card['name_last'] = "zhu"
-    andrew_card['handle_str'] = andrew_profile['user']['handle_str']
-
-    # chris's expected info
-    chris_card = {}
-    chris_card['u_id'] = user_chris['u_id']
-    chris_card['email'] = "chrisc@gmail.com"
-    chris_card['name_first'] = "chris"
-    chris_card['name_last'] = "zhu"
-    chris_card['handle_str'] = chris_profile['user']['handle_str']
-
-    assert andrew_card in users_card['users']
-    assert chris_card in users_card['users']
+    for user in users:
+        if user['u_id']  == user_andrew['u_id']:
+           assert user['email'] == "andrewt@gmail.com"
+           assert user['name_first'] == "andrew"
+           assert user['name_last'] == "taylor"
+        else:
+           assert user['email'] == "chrisc@gmail.com"
+           assert user['name_first'] == "chris"
+           assert user['name_last'] == "chen"
+ 
