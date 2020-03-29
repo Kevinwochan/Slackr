80f23eec891e3d27918b8ad64d5ff9f88767b1c7
Michael <michael.liang1110@gmail.com>
2020-03-29 19:20:05 +1100

    basic error tests complete

--- src/IntegrationTests/admin_test.py
+++ src/IntegrationTests/admin_test.py
@@ -1,7 +1,7 @@
 import pytest
 from src.error import InputError, AccessError
 from src.admin import permission_change
-from src.auth import auth_register
+from src.auth import auth_register, auth_logout
 
 @pytest.fixture
 def new_user():
@@ -16,18 +16,27 @@ def new_user_2():
                          "first_name2", "last_name2")
 
 
-def test_invalid_token(new_user_2):
+def test_invalid_token(new_user, new_user_2):
+    '''tests that permission_change throws an AccessError when it's given an invalid token'''
+    auth_logout(new_user['token']) #invalidating token by logging out
     with pytest.raises(AccessError):
-        assert permission_change(-1, new_user_2['u_id'], 1)
+        permission_change(new_user['u_id'], new_user_2['u_id'], 1)
 
-def test_():
+def test_invalid_permission_id(new_user, new_user_2):
+    '''tests that permission_change throws an InputError when it's given an invalid permission_id'''
+    with pytest.raises(InputError):
+        permission_change(new_user['token'], new_user_2['u_id'], 3)
+
+def test_invalid_user(new_user):
+    '''tests that permission_change throws an InputError when it's given an invalid u_id'''
+    with pytest.raises(InputError):
+        permission_change(new_user['token'], -1, 1)
+
+def test_member_becoming_owner():
     pass   
 
-def test_():
+def test_owner_becoming_member():
     pass   
 
-def test_():
-    pass   
-
-def test_():
-    pass   
+def test_no_permission_change():
+    pass
