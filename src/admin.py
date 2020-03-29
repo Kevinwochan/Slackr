01c236afb9c8b4a97d6e7ef274f1930240086006
Michael <michael.liang1110@gmail.com>
2020-03-29 16:39:59 +1100

    pylint 10/10

--- src/admin.py
+++ src/admin.py
@@ -6,8 +6,8 @@ from src.utils import check_token
 from src.global_variables import get_slackr_owners, get_users
 from src.channel import is_valid_user
 
-def permission_change(token, u_id, permission_id): #change a user's permissions on the server
-    
+def permission_change(token, u_id, permission_id):
+    '''change a user's permissions on the server'''
     user_id = check_token(token)
 
     if not is_valid_user(u_id):
@@ -18,10 +18,11 @@ def permission_change(token, u_id, permission_id): #change a user's permissions 
 
     if not user_id in get_slackr_owners():
         raise AccessError
-    
     # all possible errors raised.
     if permission_id == 1:
         get_users()[u_id]['is_owner'] = True
-        
     elif permission_id == 2:
         get_users()[u_id]['is_owner'] = False
+
+    return {}
+    
