9f47c2c5eb20e2235f78cc6c547b4b5ba4ee1512
Michael <michael.liang1110@gmail.com>
2020-03-29 19:50:49 +1100

    tested most if not all errors

--- src/IntegrationTests/admin_test.py
+++ src/IntegrationTests/admin_test.py
@@ -15,12 +15,24 @@ def new_user_2():
     return auth_register("z2222222@unsw.edu.au", "password2",
                          "first_name2", "last_name2")
 
+@pytest.fixture
+def new_user_3():
+    '''create a new user'''
+    return auth_register("z3333333@unsw.edu.au", "password3",
+                         "first_name3", "last_name3")
+
 
 def test_invalid_token(new_user, new_user_2):
     '''tests that permission_change throws an AccessError when it's given an invalid token'''
     auth_logout(new_user['token']) #invalidating token by logging out
     with pytest.raises(AccessError):
-        permission_change(new_user['u_id'], new_user_2['u_id'], 1)
+        permission_change(new_user['token'], new_user_2['u_id'], 1)
+
+def test_not_owner(new_user, new_user_2, new_user_3):
+    '''tests that a normal user cannot change permission - gives access error'''
+    with pytest.raises(AccessError):
+        permission_change(new_user_2['token'], new_user_3['u_id'], 1)
+
 
 def test_invalid_permission_id(new_user, new_user_2):
     '''tests that permission_change throws an InputError when it's given an invalid permission_id'''
