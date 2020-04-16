b8f4a9d0509dfd47034f8d30fbb020d914b03114
Lorenzo Lee Solano <LorenzoLeeSolano@gmail.com>
2020-04-16 19:08:14 +1000

    added message to emails, cleaned up code slightly - passwordreset

--- src/auth_passwordreset.py
+++ src/auth_passwordreset.py
@@ -1,5 +1,9 @@
+'''Provides password reset functionality to slackr.
+all generated reset tokens expire after approximately 20min
+'''
 from hashlib import sha256
-import smtplib, ssl
+import smtplib
+import ssl
 from src.auth_helper import is_password_valid, find_id, is_email_unique
 from src.utils import generate_reset_code, check_reset_code
 from src.error import InputError
@@ -9,22 +13,62 @@ PORT = 465
 SLACKR_EMAIL = 'slackrmehk@gmail.com'
 PASSWORD = 'nashe4-riqnav-Jaqfur'
 
-#Time before reset_code expires (20min)
-VALID_DURATION = 20*60
+#Time before reset_code expires (10min)
+VALID_DURATION = 10*60
 
-def send_passwordreset_email(recipient_email, reset_code):
+def send_email(recipient_email, message):
+    '''sends an email to recipient_email containing message
+
+    :param recipient_email: email address to recieve the email
+    :type recipient_email: str
+    :param message: message contained in the email
+    :type message: str
+    '''
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL("smtp.gmail.com", PORT, context=context) as server:
         server.login(SLACKR_EMAIL, PASSWORD)
-        server.sendmail(SLACKR_EMAIL, recipient_email, reset_code)
+        server.sendmail(SLACKR_EMAIL, recipient_email, message)
+
+def create_reset_email(recipient_email, reset_code):
+    '''Returns message to send as an email
+
+    :param recipient_email: email address to recieve the email
+    :type recipient_email: str
+    :param reset_code: jwt token
+    :type reset_code: str
+    :return: email message
+    :rtype: str
+    '''
+
+    return f"""\
+Subject: Reset Your Password
+
+We've recieved a request to reset the password for your slackr account:
+{recipient_email}
+
+Your reset code is:
+{reset_code}
+
+Please copy and paste this to avoid mistakes.
+This code will expire after 10 minutes.
+"""
 
 
 def auth_passwordreset_request(email):
+    '''For a registered email, generate and email a reset code that expires after VALID_DURATION
+
+
+    :param email: valid email address that corresponds to a slackr user
+    :type email: str
+    :return: empty dictionary
+    :rtype: dict
+    '''
     if is_email_unique(email):
         return {}
     reset_code = generate_reset_code(email, VALID_DURATION)
-    send_passwordreset_email(email, reset_code)
+    message = create_reset_email(email, reset_code)
+    send_email(email, message)
     return {}
 
 def auth_passwordreset_reset(reset_code, new_password):
@@ -34,7 +78,8 @@ def auth_passwordreset_reset(reset_code, new_password):
     :type reset_code: str
     :param new_password: Users new password
     :type new_password: str
-    :raises InputError: Invalid reset code, expired reset code, invalid password
+    :raises InputError: Invalid reset code, expired reset code, invalid password,
+    :raises InputError: No user exists with that email
     :return: empty dictionary
     :rtype: dict
     '''
